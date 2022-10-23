688528df9213b19b57186f49df72fae110e5e5da
Steven DeTeso <sadeteso@gmail.com>
2022-10-23 13:04:08 -0400

    final commit

--- server.py
+++ server.py
@@ -1,18 +1,33 @@
-from cgitb import reset
-from flask import Flask, redirect, session, render_template #type: ignore
+from flask import Flask, redirect, session, render_template, request #type: ignore
 app = Flask(__name__)
+
+#need this app.secret_key every time we use session to enable it to work.
+app.secret_key = "asked1245"
 
 @app.route('/')
 def home():
     return redirect('/home')
 
+# /home screen renders the form.html
 @app.route('/home')
 def r_home():
     return render_template('form.html')
 
+# listener route for form data, note the methods (plural s here)
+# takes request.form data and links it to sesson data with the same name. 
+@app.route('/process', methods=['POST'])
+def process():
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comments'] = request.form['comments']
+    # redirect always on post to a /result route which will render the result.html below
+    return redirect('/result')
+
 @app.route('/result')
 def r_result():
     return render_template('result.html')
+
 
 @app.route('/form')
 def form():
